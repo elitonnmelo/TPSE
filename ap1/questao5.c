f1689f165dd299f326e25c976dbed2e5c4315a52
elitonpc\Éliton Melo <elitonp.melo@gmail.com>
2023-04-30 10:53:04 -0300

    add questao 5

--- ap1/questao5.c
+++ ap1/questao5.c
@@ -7,4 +7,20 @@
 
 int main() {
     unsigned char gpioREG;
+    //A: escreva a logica binaria para setar a variavel gpioREG com habilitado (gpio_enable)
+    gpioREG |= gpio_enable;
+    //B: escreva a logica binaria para testa se a variavel gpioREG foi definido como saida (gpio_direction), caso positivo voce deve desabilitar (gpio_enabled)
+    if (gpioREG & gpio_direction) {
+        gpioREG &= ~gpio_enabled;
+    }
+    //C: escrever a logica binária para setar a variável gpioREG como entrada (gpio_direction).
+    gpioREG = (gpioREG & ~gpio_enable) | gpio_direction;
+    //D: escrever a logica binaria para alterar o gpioREG como nivel alto (gpio_level) ou nivel baixo. Exemplo se ele estava como alto passar para baixo e vice -versa.
+    // Alterna o nível de gpioREG entre alto e baixo
+    if (gpioREG & gpio_level) {   // Se o nível é alto
+        gpioREG ^= gpio_level;   // Inverte o bit correspondente para baixo
+    } else {                      // Se o nível é baixo
+        gpioREG |= gpio_level;   // Altera o bit correspondente para alto
+    }
+
 }
