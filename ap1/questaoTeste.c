5a09eedbfd86eeaf8717c435ce112d440eafccc9
elitonpc\Éliton Melo <elitonp.melo@gmail.com>
2023-05-03 21:58:53 -0300

    add questao teste

---
+++ ap1/questaoTeste.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int countBits(bool w, unsigned int value) {
+    unsigned int mask = ((w == 1)? 0x0 : 0x1);
+    int count = 0;
+    for (int i = 0; i < 32; i++) {
+        if (value ^ mask) {
+            count++;
+        }
+        value >>= 1;
+       
+    }
+    return count;
+}
+
+
+
+int main() {
+    int num;
+    int num2;
+    scanf("%d%d", &num, &num2);
+    int num3 = countBits(num, num2); 
+    printf("%d\n", num3);
+
+    return 0;
+}
